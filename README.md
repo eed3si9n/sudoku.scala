e11b225973fbb7ebc81f785dbeb3b2a770ab803b
e.e d3si9n <eed3si9n@gmail.com>
2012-12-04 10:30:33 -0500

    command line

---
+++ README.md
@@ -0,0 +1,18 @@
+sudoku.scala
+============
+
+This is a sudoku solver written in Scala do demonstrate `Func` in Scalaz 7: http://eed3si9n.com/sudoku-using-func
+
+how to build
+------------
+
+use sbt.
+
+```
+> run data/3.sdk
+```
+
+license
+-------
+
+MIT License.
--- src/main/scala/com/eed3si9n/sudoku/Main.scala
+++ src/main/scala/com/eed3si9n/sudoku/Main.scala
@@ -3,5 +3,6 @@ package com.eed3si9n.sudoku
 import java.io.File
 
 object Main extends App {
-  Solver.solve(Reader.read(new File("data/2.sdk")))
+  args.headOption map { arg => Solver.solve(Reader.read(new File("data/2.sdk")))
+  } getOrElse println("usage: > run data/1.sdk")
 }
